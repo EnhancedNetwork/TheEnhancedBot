722c9673925c341eccc62281fcccea45a8c81fa1
Moe <moebstergaming@hotmail.com>
2023-12-01 22:17:07 -0500

    Updated /say to give access to staff

--- commands/owner/say.js
+++ commands/owner/say.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const con = require('../../mysqlConn.js');
 
@@ -8,9 +8,32 @@ module.exports = {
         .setDescription('Say a message')
         .addStringOption(option => option.setName('message').setDescription('The message to say').setRequired(true)),
     async execute(interaction) {
-        if(interaction.member.id != "800552171048927243") return interaction.editReply("You are not allowed to use this command");
+        if (!interaction.member.roles.cache.has('1180344568152076331'))
+            return interaction.reply({ content: "You do not have permission to use this command", ephemeral: true });
 
-        interaction.channel.send(interaction.options.getString('message'));
-        interaction.reply({ content: "Done", ephemeral: true })
+        const message = interaction.options.getString('message');
+        // Log the command usage in a separate channel
+        const logChannel = interaction.guild.channels.cache.get('1122232494402580490');
+        let logEmbed = new EmbedBuilder()
+            .setTitle(interaction.member.displayName + " used /say")
+            .setColor("#FF0000")
+            .setTimestamp()
+            .addFields([
+                { name: "Channel:", value: `<#${interaction.channel.id}>`, inline: true },
+                { name: "User:", value: `${interaction.user} (${interaction.user.id})`, inline: true },
+                { name: "Message:", value: message, inline: false },
+            ])
+            .setFooter({ text: `User ID: ${interaction.user.id}` });
+        await logChannel.send({ embeds: [logEmbed] });
+
+        // Send the message in an embed
+        let embed = new EmbedBuilder()
+            .setTitle("This message was sent by a staff member")
+            .setDescription(message)
+            .setColor("#FF0000")
+            .setTimestamp()
+            .setFooter({ text: `Town of Mods by TOHE` });
+        await interaction.channel.send({ embeds: [embed] });
+        await interaction.reply({ content: "Done", ephemeral: true })
     }
 }
