d416fb50184b792a1126ab74eeace1c181cce77c
Moe <moebstergaming@hotmail.com>
2024-05-20 23:27:46 -0400

    Updated Global Commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,13 +3,15 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = [];
+const regCommands = [];
+const toheCommands = [];
 // Grab all the command files from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
+	console.log(`Loading commands from ${folder}...`);
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
@@ -17,7 +19,10 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
+			if (folder === 'dev' || folder === 'owner' || folder === 'tohe')
+				toheCommands.push(command.data.toJSON());
+			else
+				regCommands.push(command.data.toJSON());
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -30,15 +35,21 @@ const rest = new REST().setToken(token);
 // and deploy your commands!
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${regCommands.length + toheCommands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
+			Routes.applicationCommands(clientId),
+			{ body: regCommands }
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		const toheData = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: toheCommands }
+		);
+
+		console.log(`Successfully reloaded ${data.length} global commands.`);
+		console.log(`Successfully reloaded ${toheData.length} TOHE commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
